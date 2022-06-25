aa3fa592e1a577c63494242de7073599301e9670
baishun <863548227@qq.com>
2022-06-25 10:59:34 +0800

    init

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,18 @@
 // vitepress/config.js
-
+function getTsSidebar() {
+    return [{
+        text: '基础知识',
+        children: [{
+                text: '基础',
+                link: '/ts/'
+            }, // link 和 头部导航的 link 对应， 访问文件为 docs/ts/basics.md 文件
+            {
+                text: '内置类型',
+                link: '/ts/type'
+            },
+        ]
+    }, ]
+}
 module.exports = {
     title: "我的博客", // 网站标题
     description: '我的vitepress博客.', //网站描述
@@ -16,28 +29,76 @@ module.exports = {
     // //         },
     // //     ],
     // // ],
-    // // 主题配置
+    // 主题配置
     themeConfig: {
-        // repo: 'vuejs/vitepress', // 你的 github 仓库地址，网页的右上角会跳转
+        repo: 'vuejs/vitepress', // 你的 github 仓库地址，网页的右上角会跳转
         //   头部导航
-        // nav: [{
-        //         text: '首页',
-        //         link: '/cesium/cesium.md'
-        //     },
-            
-        // ],
-        // //   侧边导航
-         sidebar: [{
-             text: "cesium",
-            //  link: '/cesium/cesium.md',
-             items: [],
-         },
-        {
-            text: "test",
-            link: '/cesium/cesium.md',
-            items: [],
-        }
-        ],
+        nav: [{
+                text: '前端技术',
+                items: [
+                    {
+                        text: 'cesium',
+                        link: '/cesium/',
+                        activeMatch: '^/cesium/'
+                    },
+                    {
+                        text: 'TS',
+                        link: '/ts/',
+                        // activeMatch: '^/ts/'
+                    },
+                    {
+                        text: 'vue',
+                        link: '/vue/'
+                    }
+                ]
+            }],
+
+
+            sidebar: {
+                '/cesium/': [{
+                        text: 'ceisum',
+                        link:'/cesium/',
+                        items:[
+                            
+                                {
+                                    text: '内置类型',
+                                    link: '/cesium/'
+                                },
+                            
+                        ]
+                    }, // link 和 头部导航的 link 对应， 访问文件为 docs/ts/basics.md 文件
+                    
+                ],
+                '/vue/': [{
+                        text: 'vue3',
+                        link: '/vue/',
+                        items: [
+
+                            {
+                                text: 'vue',
+                                link: '/vue/'
+                            },
+
+                        ]
+                    }, // link 和 头部导航的 link 对应， 访问文件为 docs/ts/basics.md 文件
+
+                ],
+                '/ts/': [{
+                        text: 'TypeScript',
+                        link: '/ts/',
+                        items: [
+
+                            {
+                                text: '内置类型',
+                                link: '/ts/type'
+                            },
+
+                        ]
+                    }, // link 和 头部导航的 link 对应， 访问文件为 docs/ts/basics.md 文件
+
+                ]
+            },
+
         
     }
 }
